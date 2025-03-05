9a06482fc002943da5759f58928aea9875548ada
NileshBalu <nileshbalu2002@gmail.com>
2025-03-05 16:51:54 +0100

    latest working code

--- src/roboteq_controller/src/speed_control_node.cpp
+++ src/roboteq_controller/src/speed_control_node.cpp
@@ -28,8 +28,8 @@ class SpeedControlNode : public rclcpp::Node
             rc_input_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("rc_input", 10);
             RCLCPP_INFO(this->get_logger(), "Publisher created");
             wheel_speed_subscriber_ = this->create_subscription<std_msgs::msg::Float64MultiArray>("target_speed", 10, std::bind(&SpeedControlNode::set_speed, this, std::placeholders::_1));
-            publish_timer_ = this->create_wall_timer(500ms, std::bind(&SpeedControlNode::publish_info, this));
-            read_timer_ = this->create_wall_timer(10ms, std::bind(&SpeedControlNode::read_info, this));
+            publish_timer_ = this->create_wall_timer(10ms, std::bind(&SpeedControlNode::publish_info, this));
+            read_timer_ = this->create_wall_timer(5ms, std::bind(&SpeedControlNode::read_info, this));
             RCLCPP_INFO(this->get_logger(), "Timer created");
             freq_timer_ = this->create_wall_timer(1s, std::bind(&SpeedControlNode::stream_rate, this));
             
@@ -80,7 +80,7 @@ class SpeedControlNode : public rclcpp::Node
                     RCLCPP_INFO(this->get_logger(), "Sending Query %s", echo_query.c_str());
                     serial_->write(echo_query);
 
-                    std::string speed_query = "/\"d=\",\":\"?S " + std::to_string(motor_1_channel_) + "_?S " + std::to_string(motor_2_channel_) + "_?PI 1_?PI 2_# 10\r\n";
+                    std::string speed_query = "/\"d=\",\":\"?S " + std::to_string(motor_1_channel_) + "_?S " + std::to_string(motor_2_channel_) + "_?PI 1_?PI 2_# 5\r\n";
                     RCLCPP_INFO(this->get_logger(), "Sending Query %s", speed_query.c_str());
                     serial_->write(speed_query);
                     
@@ -124,39 +124,40 @@ class SpeedControlNode : public rclcpp::Node
 
                 serial_->write(speed_command);
                 serial_->flush();
-                RCLCPP_INFO(this->get_logger(), "Sent: %s", speed_command.c_str());
+                serial_->flushInput();
+                // RCLCPP_INFO(this->get_logger(), "Sent: %s", speed_command.c_str());
                 // RCLCPP_INFO(this->get_logger(), "Received target: %d, %d", target_speed_1, target_speed_2);
 
 
-                // Wait for '+' acknowledgment
-                std::string response;
-                auto start_time = std::chrono::steady_clock::now();
-                bool ack_received = false;
+                // // Wait for '+' acknowledgment
+                // std::string response;
+                // auto start_time = std::chrono::steady_clock::now();
+                // bool ack_received = false;
 
-                while (std::chrono::steady_clock::now() - start_time < std::chrono::milliseconds(500)) // 500 ms timeout
-                {
-                    if (serial_->available())
-                    {
-                        char c;
-                        serial_->read(reinterpret_cast<uint8_t*>(&c), 1);  // Cast char* to uint8_t*
-                        response += c;
+                // while (std::chrono::steady_clock::now() - start_time < std::chrono::milliseconds(500)) // 500 ms timeout
+                // {
+                //     if (serial_->available())
+                //     {
+                //         char c;
+                //         serial_->read(reinterpret_cast<uint8_t*>(&c), 1);  // Cast char* to uint8_t*
+                //         response += c;
 
-                        if (c == '+') // Check for acknowledgment
-                        {
-                            ack_received = true;
-                            break;
-                        }
-                    }
-                }
+                //         if (c == '+') // Check for acknowledgment
+                //         {
+                //             ack_received = true;
+                //             break;
+                //         }
+                //     }
+                // }
 
-                if (ack_received)
-                {
-                    RCLCPP_INFO(this->get_logger(), "Acknowledgment received: +");
-                }
-                else
-                {
-                    RCLCPP_WARN(this->get_logger(), "No acknowledgment received within timeout");
-                }
+                // if (ack_received)
+                // {
+                //     RCLCPP_INFO(this->get_logger(), "Acknowledgment received: +");
+                // }
+                // else
+                // {
+                //     RCLCPP_WARN(this->get_logger(), "No acknowledgment received within timeout");
+                // }
             }
             else
             {
@@ -169,8 +170,8 @@ class SpeedControlNode : public rclcpp::Node
         {
             if(check_connection())
             {
-                serial_->flushInput();
-                std::string response = serial_->readline(100, "\r");
+                // serial_->flushInput();
+                std::string response = serial_->readline(20, "\r");
                 // Check if it starts with prefix
                 if(response.find(prefix_) == 0)
                 {
@@ -180,7 +181,7 @@ class SpeedControlNode : public rclcpp::Node
                 }
                 else
                 {
-                    RCLCPP_ERROR(this->get_logger(), "Invalid response: %s", response.c_str());
+                    // RCLCPP_ERROR(this->get_logger(), "Invalid response: %s", response.c_str());
                 }
             }
             else
@@ -196,8 +197,8 @@ class SpeedControlNode : public rclcpp::Node
             if(check_connection())
             {
 
-                // try
-                // {
+                try
+                {
                     // RCLCPP_INFO(this->get_logger(), "%s",serial_response_.c_str());
                     motor_info = parse_string(serial_response_);
                     // RCLCPP_INFO(this->get_logger(), "Left: %d, Right: %d", motor_info.motor_1_speed, motor_info.motor_2_speed);
@@ -210,11 +211,11 @@ class SpeedControlNode : public rclcpp::Node
                     rc_input_publisher_->publish(rc_input_msg);
                     wheel_speed_publisher_->publish(wheel_speed_msg);
                     // RCLCPP_INFO(this->get_logger(), "Speed published");
-                // }
-                // catch(const std::exception& e)
-                // {
-                //     RCLCPP_ERROR(this->get_logger(), "Error parsing speed: %s", e.what());
-                // }
+                }
+                catch(const std::exception& e)
+                {
+                    RCLCPP_ERROR(this->get_logger(), "Error parsing speed: %s", e.what());
+                }
             }
             else
             {
